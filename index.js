05618d1690f01cd56641804ec82d757a6ceb9577
louriccia <louriccia@gmail.com>
2021-03-15 03:25:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -30,11 +30,14 @@ ref.on("value", function(snapshot) {
     console.log("The read failed: " + errorObject.code);
 });*/
 var secondaryAppConfig = {
-    projectId: "botto-efbfd",
-    appId: "1:131908843411:web:9b64e1375087fb07f91a66",
     apiKey: "AIzaSyCEJX5k_LmFjPxV-1IQYZNESC3apL62onM",
+    authDomain: "botto-efbfd.firebaseapp.com",
     databaseURL: "https://botto-efbfd.firebaseio.com",
-    storageBucket: "botto-efbfd.appspot.com"
+    projectId: "botto-efbfd",
+    storageBucket: "botto-efbfd.appspot.com",
+    messagingSenderId: "131908843411",
+    appId: "1:131908843411:web:9b64e1375087fb07f91a66",
+    measurementId: "G-BQHFL0GVF1"
 };
 const app2 = firebase.initializeApp(secondaryAppConfig, "secondary");
 }
