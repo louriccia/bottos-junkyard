cb5fd518cbd77b6f038ffe1df777a8f1ef496f1c
louriccia <louriccia@gmail.com>
2021-03-15 02:20:07 -0700

    test

---
+++ main.js
@@ -0,0 +1,26 @@
+var firebase = require("firebase/app");
+require('firebase/auth');
+require('firebase/database');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCEJX5k_LmFjPxV-1IQYZNESC3apL62onM",
+    authDomain: "botto-efbfd.firebaseapp.com",
+    databaseURL: "https://botto-efbfd.firebaseio.com",
+    projectId: "botto-efbfd",
+    storageBucket: "botto-efbfd.appspot.com",
+    messagingSenderId: "131908843411",
+    appId: "1:131908843411:web:9b64e1375087fb07f91a66",
+    measurementId: "G-BQHFL0GVF1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  var ref = database.ref('challenge/profiles');
+ref.on("value", function(snapshot) {
+    challengedata = snapshot.val();
+    console.log(Object.keys(snapshot))
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
