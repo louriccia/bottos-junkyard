8ec783ad2fb3c7bc2010d6ff492c1abea589b3cb
louriccia <louriccia@gmail.com>
2021-03-15 02:33:20 -0700

    firebase

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCEJX5k_LmFjPxV-1IQYZNESC3apL62onM",
-    authDomain: "botto-efbfd.firebaseapp.com",
-    databaseURL: "https://botto-efbfd.firebaseio.com",
-    projectId: "botto-efbfd",
-    storageBucket: "botto-efbfd.appspot.com",
-    messagingSenderId: "131908843411",
-    appId: "1:131908843411:web:9b64e1375087fb07f91a66",
-    measurementId: "G-BQHFL0GVF1"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 
+  // Initialize Firebase
   var database = firebase.database();
 
   var ref = database.ref('challenge/profiles');
