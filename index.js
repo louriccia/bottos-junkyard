bde73f2048436cff19c4e4fda2e23b31f235e2f2
louriccia <louriccia@gmail.com>
2021-03-15 03:06:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ function setup(){
     appId: "1:131908843411:web:9b64e1375087fb07f91a66",
     measurementId: "G-BQHFL0GVF1"
   };
+  var secondaryAppConfig = {
+    projectId: "botto-efbfd",
+    appId: "1:131908843411:web:9b64e1375087fb07f91a66",
+    apiKey: "AIzaSyCEJX5k_LmFjPxV-1IQYZNESC3apL62onM",
+    databaseURL: "https://botto-efbfd.firebaseio.com",
+    storageBucket: "botto-efbfd.appspot.com"
+};
+  var firebase = firebase.initializeApp(secondaryAppConfig, "secondary")
   var database = firebase.database();
 
   var ref = database.ref('challenge/profiles');
