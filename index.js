64990f7f1f83f328a3a51d78ba418d8cb79be922
louriccia <louriccia@gmail.com>
2021-03-15 03:18:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function setup(){
   // Initialize Firebase
   console.log("ready")
+/*
   var firebaseConfig = {
     apiKey: "AIzaSyCEJX5k_LmFjPxV-1IQYZNESC3apL62onM",
     authDomain: "botto-efbfd.firebaseapp.com",
@@ -27,7 +28,34 @@ ref.on("value", function(snapshot) {
     console.log(Object.keys(snapshot))
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
-});
+});*/
+const app2 = firebase.initializeApp({
+    databaseURL: "https://botto-efbfd.firebaseio.com"
+  }, 'app2');
+}
+// test default
+print('Creating db ref using DEFAULT');
+try {
+  const db1 = firebase.database();
+  print('Success!');
+}
+catch(e) {
+  print('ERROR: ' + e);
+}
+
+// test URL
+print('Creating db ref using URL');
+try {
+  const db2 = firebase.database(app2);
+  print('Success!');
+}
+catch(e) {
+  print('ERROR: ' + e);
+}
+
+function print(txt) {
+  const $pre = $('pre');
+  $pre.text( $pre.text() + txt + "\n");
 }
 
 setup()
