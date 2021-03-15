b89537843b951efc121f882a8ccf8b8fa2a57027
louriccia <louriccia@gmail.com>
2021-03-15 02:48:09 -0700

    test

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
 function setup(){
   // Initialize Firebase
+  var firebaseConfig = {
+    apiKey: "AIzaSyCEJX5k_LmFjPxV-1IQYZNESC3apL62onM",
+    authDomain: "botto-efbfd.firebaseapp.com",
+    databaseURL: "https://botto-efbfd.firebaseio.com",
+    projectId: "botto-efbfd",
+    storageBucket: "botto-efbfd.appspot.com",
+    messagingSenderId: "131908843411",
+    appId: "1:131908843411:web:9b64e1375087fb07f91a66",
+    measurementId: "G-BQHFL0GVF1"
+  };
+  firebase.initializeApp(firebaseConfig);
   var database = firebase.database();
 
   var ref = database.ref('challenge/profiles');
@@ -11,3 +22,5 @@ ref.on("value", function(snapshot) {
     console.log("The read failed: " + errorObject.code);
 });
 }
+
+setup()
